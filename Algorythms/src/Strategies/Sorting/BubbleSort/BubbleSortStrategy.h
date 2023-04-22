43a4562c49392c732811682e68ba31c53c7d6c02
TsepilovDanil <tsepilov.danil@yandex.ru>
2023-04-22 16:16:02 +0300

    фиксация перед переводом алгоритмов стратегий в виртуальные функции (забыл сохранить один файл)

--- Algorythms/src/Strategies/Sorting/BubbleSort/BubbleSortStrategy.h
+++ Algorythms/src/Strategies/Sorting/BubbleSort/BubbleSortStrategy.h
@@ -27,15 +27,7 @@ template<typename Type, std::size_t Count>
 void BubbleSortStrategy<Type, Count>::StartWork()
 {
 	std::shared_ptr<ArrayStorage<Type, Count>> storage = std::make_shared<ArrayStorage<Type, Count>>();
-
-	//try
-	{
-		auto strategyThread = std::thread(&BubbleSortStrategy::algorythm<Type, Count>, this, (ArrayStorage<Type, Count> *) & storage);
-	}
-	//catch (const std::exception& exception)
-	{
-		//std::cout << "Cant start Strategy thread" << std::endl;
-	}
+	auto strategyThread = std::thread(&BubbleSortStrategy::algorythm<Type, Count>, this, (ArrayStorage<Type, Count> *) & storage);
 }
 
 
