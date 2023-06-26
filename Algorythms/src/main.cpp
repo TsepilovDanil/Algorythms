ce3942d0c59802b522f6036c76104f4acc3408dd
TsepilovDanil <tsepilov.danil@yandex.ru>
2023-06-26 22:33:15 +0300

    небольшой рефакторинг

--- Algorythms/src/main.cpp
+++ Algorythms/src/main.cpp
@@ -4,23 +4,23 @@
 int main(int argc, const std::size_t* argv[])
 {
     std::initializer_list<int> list{ 9,8,7,6,5,4,3,2,1 };
-
+    
     if (!list.size())
         return 0;
 
-    std::shared_ptr <CompositionsFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>> bubbleSortFabric = std::make_shared<BubbleSortFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(std::forward<std::initializer_list<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(list));
+    auto bubbleSortFabric = std::make_shared<BubbleSortFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(std::forward<std::initializer_list<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(list));
     bubbleSortFabric->GetComposition()->StartAlgorythm();
 
-	std::shared_ptr <CompositionsFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>> insertionSortFabric = std::make_shared<InsertionSortFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(std::forward<std::initializer_list<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(list));
+	auto insertionSortFabric = std::make_shared<InsertionSortFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(std::forward<std::initializer_list<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(list));
     insertionSortFabric->GetComposition()->StartAlgorythm();
 
-	std::shared_ptr <CompositionsFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>> mergeSortFabric = std::make_shared<MergeSortFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(std::forward<std::initializer_list<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(list));
+    auto mergeSortFabric = std::make_shared<MergeSortFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(std::forward<std::initializer_list<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(list));
     mergeSortFabric->GetComposition()->StartAlgorythm();
 
-	std::shared_ptr <CompositionsFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>> quickSortFabric = std::make_shared<QuickSortFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(std::forward<std::initializer_list<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(list));
+    auto quickSortFabric = std::make_shared<QuickSortFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(std::forward<std::initializer_list<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(list));
 	quickSortFabric->GetComposition()->StartAlgorythm();
 
-	std::shared_ptr <CompositionsFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>> selectionSortFabric = std::make_shared<SelectionSortFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(std::forward<std::initializer_list<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(list));
+    auto selectionSortFabric = std::make_shared<SelectionSortFabric<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(std::forward<std::initializer_list<std::remove_reference<decltype(*const_cast<int*>(list.begin()))>::type>>(list));
 	selectionSortFabric->GetComposition()->StartAlgorythm();
 
 }
